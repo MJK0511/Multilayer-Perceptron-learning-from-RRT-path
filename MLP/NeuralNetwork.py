fd0fae63f943564a14640b8c233f08446fd32a7f
MJK0511 <minjek0511@gmail.com>
2023-12-04 23:42:48 +0900

    rewriting NN

--- MLP/NeuralNetwork.py
+++ MLP/NeuralNetwork.py
@@ -10,40 +10,16 @@ class neuralNetwork:
         self.hlayers = hiddenlayers
         self.onodes = outputnodes
 
-        # リンクの重み行列wih(weights input hidden)とwho(weights hidden output)
-        # 行列内の重みw_i_j, ノードiから次の層のノードjへのリンクの重み
-        # He
-        self.wih1 = np.random.randn(self.hnodes, self.inodes) * np.sqrt(1.0 / self.inodes)
-        self.wih2 = np.random.randn(self.hnodes, self.hnodes) * np.sqrt(1.0 / self.inodes)
-        self.who = np.random.randn(self.onodes, self.hnodes) * np.sqrt(1.0 / self.hnodes)
-        #bias 
-        self.bias_h1 = np.zeros((self.hnodes, 1))
-        self.bias_h2 = np.zeros((self.hnodes, 1))
-        self.bias_o = np.zeros((self.onodes, 1))
-        
-        # 学習率の設定
-        self.lr = learningrate
-
-        # 活性化関数 
-        self.activation_function = self.sigmoid
+        self.initialize_weights_and_biases()    # 重みとバイアスの初期化
+        self.lr = learningrate                  # 学習率の設定
+        self.activation_function = self.sigmoid # 活性化関数 
 
         # Adam optimizer for weights
         self.optimizer_wih = Adam(self.wih1.shape, learning_rate=self.lr)
         self.optimizer_who = Adam(self.who.shape, learning_rate=self.lr)
+
         pass
     
-    def sigmoid(self, x):
-        return np.exp(-np.abs(x)) / (1 + np.exp(-np.abs(x)))
-    
-    
-    def identity(self, x):
-        return x
-    
-    def mse_loss(self, targets, outputs):
-        result = np.mean((targets - outputs) **2)
-        return np.full((targets.shape[0], 1), result)
-        # return targets - outputs
-
     #ニューラルネットワークの学習
     def train(self, inputs_list, targets_list):
         #入力値とターゲット値を2D配列に変換
@@ -60,30 +36,44 @@ class neuralNetwork:
         hidden_inputs2 = np.dot(self.wih2, hidden_outputs1) + self.bias_h2
         hidden_outputs2 = self.activation_function(hidden_inputs2)
 
+        hidden_inputs3 = np.dot(self.wih3, hidden_outputs2) + self.bias_h3
+        hidden_outputs3 = self.activation_function(hidden_inputs3)
+
+        hidden_inputs4 = np.dot(self.wih4, hidden_outputs3) + self.bias_h4
+        hidden_outputs4 = self.activation_function(hidden_inputs4)
+
         # 出力層に入ってくる信号の計算
-        final_inputs = np.dot(self.who, hidden_outputs2) + self.bias_o
+        final_inputs = np.dot(self.who, hidden_outputs4) + self.bias_o
         final_outputs = self.activation_function(final_inputs)
        
         # 出力層の誤差
         output_errors = self.mse_loss(targets, final_outputs)
 
         # 隠れ層の誤差は出力層の誤差をリンクの重みの割合で分配
-        hidden_errors2 = np.dot(self.who.T, output_errors)
+        hidden_errors4 = np.dot(self.who.T, output_errors)
+        update_wih4 = self.lr * (np.dot((hidden_errors4 * hidden_outputs4 * (1.0 - hidden_outputs4)), hidden_outputs3.T))
+        self.wih4 += update_wih4
+
+        hidden_errors3 = np.dot(self.wih4.T, hidden_errors4)
+        update_wih3 = self.lr * (np.dot((hidden_errors3 * hidden_outputs3 * (1.0 - hidden_outputs3)), hidden_outputs2.T))
+        self.wih3 += update_wih3
+
+        hidden_errors2 = np.dot(self.wih3.T, hidden_errors3)
+        update_wih2 = self.lr * (np.dot((hidden_errors2 * hidden_outputs2 * (1.0 - hidden_outputs2)), hidden_outputs1.T))
+        self.wih2 += update_wih2
+
         hidden_errors1 = np.dot(self.wih2.T, hidden_errors2)
-
-
-        # 隠れ層と出力層の間のリンクの重みを更新
-        # self.who += self.lr * np.dot((output_errors * final_outputs * (1.0 - final_outputs)), hidden_outputs.T)
-        self.wih1 += self.optimizer_wih.update(np.dot((hidden_errors1 * hidden_outputs1 * (1.0 - hidden_outputs1)), inputs.T))
-        self.wih2 += self.optimizer_wih.update(np.dot((hidden_errors2 * hidden_outputs2 * (1.0 - hidden_outputs2)), hidden_outputs1.T))
-
-        self.bias_o += self.lr * output_errors  # 出力層バイアス更新
+        update_wih1 = self.lr * (np.dot((hidden_errors1 * hidden_outputs1 * (1.0 - hidden_outputs1)), inputs.T))
+        self.wih1 += update_wih1
 
         # 入力層と隠れ層の間のリンクの重みを更新
-        # self.wih += self.lr * np.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), inputs.T)
-        self.who += self.optimizer_who.update(np.dot((output_errors * final_outputs * (1.0 - final_outputs)), hidden_outputs2.T))
-        self.bias_h2 += self.lr * hidden_errors2  # 隠れ層バイアス更新
-        self.bias_h1 += self.lr * hidden_errors1
+        self.who += self.lr * (np.dot((output_errors * final_outputs * (1.0 - final_outputs)), hidden_outputs4.T))
+        
+        self.bias_o += self.lr * output_errors  # 出力層バイアス更新
+        self.bias_h1 += self.lr * hidden_errors1 # 隠れ層バイアス更新
+        self.bias_h2 += self.lr * hidden_errors2
+        self.bias_h3 += self.lr * hidden_errors3
+        self.bias_h4 += self.lr * hidden_errors4
 
         pass
     
@@ -93,9 +83,13 @@ class neuralNetwork:
         inputs = np.array(inputs_list, ndmin=2).T
         
         # 隠れ層に入ってくる信号の計算
-        hidden_inputs = np.dot(self.wih, inputs)
+        hidden_layer1 = np.dot(self.wih1, inputs)
+        hidden_layer2 = np.dot(self.wih2, hidden_layer1)
+        hidden_layer3 = np.dot(self.wih3, hidden_layer2)
+        hidden_layer4 = np.dot(self.wih4, hidden_layer3)
+
         # 隠れ層で結合された信号を活性化関数により出力
-        hidden_outputs = self.activation_function(hidden_inputs)
+        hidden_outputs = self.activation_function(hidden_layer4)
 
         # 出力層に入ってくる信号の計算
         final_inputs = np.dot(self.who, hidden_outputs)
@@ -104,3 +98,27 @@ class neuralNetwork:
         final_outputs = np.round(self.activation_function(final_inputs)*100)
         
         return final_outputs
+    
+
+    def sigmoid(self, x):
+        return np.exp(-np.abs(x)) / (1 + np.exp(-np.abs(x)))
+    
+    def identity(self, x):
+        return x
+    
+    def mse_loss(self, targets, outputs):
+        result = np.mean((targets - outputs) **2)
+        return np.full((targets.shape[0], 1), result)
+    
+    def initialize_weights_and_biases(self):
+        # 初期化関数
+        def initialize_matrix(rows, cols):
+            return np.random.randn(rows, cols) * np.sqrt(1.0 / cols)
+
+        # 重みとバイアスの初期化
+        for i in range(1, self.hlayers + 2):
+            setattr(self, f'wih{i}', initialize_matrix(self.hnodes, self.hnodes if i > 1 else self.inodes))
+            setattr(self, f'bias_h{i}', np.zeros((self.hnodes, 1)))
+
+        self.who = initialize_matrix(self.onodes, self.hnodes)
+        self.bias_o = np.zeros((self.onodes, 1))
--- MLP/main.py
+++ MLP/main.py
@@ -1,5 +1,7 @@
 from Training import MLP3rd
 from Data import call_LD, call_TD
+
+
 mlp_instance1 = MLP3rd()
 mlp_instance2 = MLP3rd()
 mlp_instance3 = MLP3rd()
@@ -7,20 +9,19 @@ mlp_instance3 = MLP3rd()
 ######################################
 # 学習
 
-#MLP1
-# input_filename1 = 'learning_input_data_m1.csv'
-# output_filename1 = 'learning_output_data_m1.csv'
-# learningdata1 = call_LD(input_filename1, output_filename1)
-# mlp_instance1.LearningData(learningdata1)
-# mlp_instance1.Learning()
+# MLP1
+input_filename1 = 'learning_input_data_m1.csv'
+output_filename1 = 'learning_output_data_m1.csv'
+learningdata1 = call_LD(input_filename1, output_filename1)
+mlp_instance1.LearningData(learningdata1)
+mlp_instance1.Learning()
 
-# #MLP2
-# input_filename2 = 'learning_input_data_m2.csv'
-# output_filename2 = 'learning_output_data_m2.csv'
-# learningdata2 = call_LD(input_filename2, output_filename2)
-
-# mlp_instance2.LearningData(learningdata2)
-# mlp_instance2.Learning()
+#MLP2
+input_filename2 = 'learning_input_data_m2.csv'
+output_filename2 = 'learning_output_data_m2.csv'
+learningdata2 = call_LD(input_filename2, output_filename2)
+mlp_instance2.LearningData(learningdata2)
+mlp_instance2.Learning()
 
 # #MLP3
 input_filename3 = 'learning_input_data_m3.csv'
@@ -37,12 +38,12 @@ testdata = call_TD(test_input_filename)
 #MLP1
 testdata1 = testdata.test_data
 print("Result1")
-result1 = mlp_instance3.TestData(testdata1)
+result1 = mlp_instance1.TestData(testdata1)
 
 #MLP2
 test_data_df2 = mlp_instance2.TestDataRE(result1[0])
 print("Result2")
-result2 = mlp_instance3.TestData(test_data_df2)
+result2 = mlp_instance2.TestData(test_data_df2)
 
 # #MLP3
 test_data_df3 = mlp_instance3.TestDataRE(result1[1])
